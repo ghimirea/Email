1a458b1a0b0722ac6e8c5fd04537f33a6c2ab736
ghimirea <aghimire85@gmail.com>
2024-07-29 11:02:14 -0400

    Checking deployment

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send({ hi: "there" });
+  res.send({ hi: "Render" });
 });
 
 app.listen(5000, () => {
